9d24c88b64f5b789ebab1c6b72de5466c88dfdd1
Irian <icortesdev@proton.me>
2025-03-06 20:33:25 +0100

    Agregar funcionalidad para eliminar imágenes de productos y ajustar la gestión de imágenes en las páginas de administración y productos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 import { loginUser, logout, registerUser } from './auth';
 import { loadProductsFromCart } from './cart/load-products-from-cart.action';
 import { createUpdateProduct } from './products/create-update-product.action';
+import { deleteProductImage } from './products/delete-product-image.action';
 import { getProductBySlug } from './products/get-product-by-slug.action';
 import { getProductsByPage } from './products/get-products-by-page.action';
 
@@ -21,5 +22,5 @@ export const server = {
 //Admin
 //Product
 createUpdateProduct,
-
+deleteProductImage
 };
